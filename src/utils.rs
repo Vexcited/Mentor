b74c70f949ddb71aae46f8db022268e2cf26b2c2
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-05 17:45:36 +0200

    feat(utils): add read file utility

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
+use anyhow::Result;
+use std::{fs::File, io, io::Read};
+
 pub fn find_between(content: &str, start: &str, end: &str) -> String {
   let start_index = content.find(start).unwrap();
   let start_index = start_index + start.len();
@@ -6,4 +9,15 @@ pub fn find_between(content: &str, start: &str, end: &str) -> String {
   let end_index = start_index + end_index;
 
   content[start_index..end_index].to_string()
+}
+
+pub fn open_readme() -> io::Result<File> {
+  File::open("README.md")
+}
+
+pub fn read_file(file: &mut File) -> Result<String> {
+  let mut buffer = String::new();
+  file.read_to_string(&mut buffer)?;
+
+  Ok(buffer)
 }
