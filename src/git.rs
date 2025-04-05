4e4c2f6f8b91b6ffed18ece371f220d8ef22e92c
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-05 16:31:39 +0200

    chore: rename branch names

--- src/git.rs
+++ src/git.rs
@@ -1,7 +1,7 @@
-use std::process::{Command, Output};
 use colored::Colorize;
+use std::process::{Command, Output};
 
-pub fn git (args: &[&str]) -> Output {
+pub fn git(args: &[&str]) -> Output {
   let log = format!("+> git {}", args.join(" "));
   println!("{}", log.bright_black());
 
@@ -12,14 +12,14 @@ pub fn git (args: &[&str]) -> Output {
 }
 
 // get every commit message between the two versions
-pub fn diff (branch_name: &str, old_version: &str, new_version: &str) -> String {
-  let references = format!("{branch_name}-v{old_version}..{branch_name}-v{new_version}");
+pub fn diff(old_version: &str, new_version: &str) -> String {
+  let references = format!("{old_version}..{new_version}");
 
   let output = git(&["log", "--oneline", "--pretty=format:%s (%h)", &references]);
   String::from_utf8_lossy(&output.stdout).to_string()
 }
 
-pub fn origin_url () -> String {
+pub fn origin_url() -> String {
   let output = git(&["remote", "get-url", "origin"]);
   let url = String::from_utf8_lossy(&output.stdout);
 
