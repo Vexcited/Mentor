a923d54a415b0ce3901d790fa79daea644afdf37
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-05 17:32:40 +0200

    feat(js): use bun instead of pnpm for checks, implement `Result` instead of unwrapping

--- src/implementations/js.rs
+++ src/implementations/js.rs
@@ -1,56 +1,86 @@
+use anyhow::Result;
 use std::{
   fs::File,
+  io,
   io::{Read, Write},
   process::Command,
 };
 
-/// Concerning JS/TS implementations, we should only run `pnpm run checks` command.
-/// It'll run `tsc`, `eslint` and sometimes some tests if they're available.
-pub fn run_checks() {
-  let output = Command::new("pnpm")
-    .arg("run")
-    .arg("checks")
-    .output()
-    .expect("failed to run pnpm command, make sure pnpm is installed globally on your machine");
+pub fn run_checks() -> Result<()> {
+  // We're checking the code style of the project.
+  let output = Command::new("bun").arg("eslint").output()?;
 
   if !output.status.success() {
     let error = String::from_utf8_lossy(&output.stdout);
-    panic!("failed to run `pnpm run checks` command, see the following stack trace:\n\n{error}");
+    return Err(anyhow::anyhow!(
+      "failed to check codestyle, see the following stack trace:\n\n{error}"
+    ));
   }
+
+  // We're checking the types of the project.
+  let output = Command::new("bun").arg("tsc").arg("--noEmit").output()?;
+
+  if !output.status.success() {
+    let error = String::from_utf8_lossy(&output.stdout);
+    return Err(anyhow::anyhow!(
+      "failed to check types, see the following stack trace:\n\n{error}"
+    ));
+  }
+
+  // We're checking the tests of the project.
+  let output = Command::new("bun").arg("test").output()?;
+
+  if !output.status.success() {
+    let error = String::from_utf8_lossy(&output.stdout);
+    return Err(anyhow::anyhow!(
+      "failed to pass tests, see the following stack trace:\n\n{error}"
+    ));
+  }
+
+  Ok(())
 }
 
-fn open_package_json() -> File {
-  File::open("package.json").unwrap()
+pub fn open_package_json() -> io::Result<File> {
+  File::open("package.json")
 }
 
 /// Reads the `package.json` file and parses it as JSON
 /// and returns the value of the `version` property as string.
-pub fn get_current_version() -> String {
-  let file = open_package_json();
+pub fn get_current_version() -> Result<String> {
+  let file = open_package_json()?;
 
-  let json: serde_json::Value = serde_json::from_reader(file).expect("file should be proper JSON");
+  let json: serde_json::Value = serde_json::from_reader(file)?;
+  let version = json.get("version");
 
-  let version = json
-    .get("version")
-    .expect("'package.json' is missing 'version' property.");
-
-  version.as_str().unwrap().to_string()
+  if let Some(version) = version {
+    if let Some(version) = version.as_str() {
+      Ok(version.to_string())
+    } else {
+      Err(anyhow::anyhow!("'version' should be a string."))
+    }
+  } else {
+    Err(anyhow::anyhow!(
+      "'package.json' is missing 'version' property."
+    ))
+  }
 }
 
 /// Edits the `package.json` file and updates the value of the `version` property.
 /// We can't use serde for this as it'll mess up the formatting.
 /// Instead, we manually replace the version in the content.
-pub fn bump_version(version: &str) {
-  let mut file = open_package_json();
+pub fn bump_version(version: &str) -> Result<()> {
+  let mut file = open_package_json()?;
 
   let mut content = String::new();
   file.read_to_string(&mut content).unwrap();
 
-  let from = format!("\"version\": \"{}\"", get_current_version());
+  let from = format!("\"version\": \"{}\"", get_current_version()?);
   let to = format!("\"version\": \"{}\"", version);
 
   let content = content.replace(&from, &to);
 
   let mut file = File::create("package.json").unwrap();
   file.write_all(content.as_bytes()).unwrap();
+
+  Ok(())
 }
