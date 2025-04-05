e1261b11233b46dee61a86e27c16c7b5e2dfd5bb
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-05 17:45:26 +0200

    refactor(js): use `ok_or_else` and `read_file` utility

--- src/implementations/js.rs
+++ src/implementations/js.rs
@@ -1,10 +1,11 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use std::{
   fs::File,
-  io,
-  io::{Read, Write},
+  io::{self, Write},
   process::Command,
 };
+
+use crate::utils::read_file;
 
 pub fn run_checks() -> Result<()> {
   // We're checking the code style of the project.
@@ -48,21 +49,15 @@ pub fn open_package_json() -> io::Result<File> {
 /// and returns the value of the `version` property as string.
 pub fn get_current_version() -> Result<String> {
   let file = open_package_json()?;
-
   let json: serde_json::Value = serde_json::from_reader(file)?;
-  let version = json.get("version");
 
-  if let Some(version) = version {
-    if let Some(version) = version.as_str() {
-      Ok(version.to_string())
-    } else {
-      Err(anyhow::anyhow!("'version' should be a string."))
-    }
-  } else {
-    Err(anyhow::anyhow!(
-      "'package.json' is missing 'version' property."
-    ))
-  }
+  let version = json
+    .get("version")
+    .ok_or_else(|| anyhow::anyhow!("'package.json' is missing 'version' property."))?
+    .as_str()
+    .ok_or_else(|| anyhow::anyhow!("'version' should be a string."))?;
+
+  Ok(version.to_string())
 }
 
 /// Edits the `package.json` file and updates the value of the `version` property.
@@ -70,17 +65,13 @@ pub fn get_current_version() -> Result<String> {
 /// Instead, we manually replace the version in the content.
 pub fn bump_version(version: &str) -> Result<()> {
   let mut file = open_package_json()?;
-
-  let mut content = String::new();
-  file.read_to_string(&mut content).unwrap();
+  let content = read_file(&mut file)?;
 
   let from = format!("\"version\": \"{}\"", get_current_version()?);
   let to = format!("\"version\": \"{}\"", version);
 
   let content = content.replace(&from, &to);
-
-  let mut file = File::create("package.json").unwrap();
-  file.write_all(content.as_bytes()).unwrap();
+  file.write_all(content.as_bytes())?;
 
   Ok(())
 }
