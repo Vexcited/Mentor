175c8787e631fa9a31791f4f52077f3f6259a49d
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-04-05 18:22:12 +0200

    fix(rust): read and update version property with `toml` crate

--- src/implementations/rust.rs
+++ src/implementations/rust.rs
@@ -1,6 +1,9 @@
 use crate::utils::read_file;
 use anyhow::Result;
-use std::{fs::File, io};
+use std::{
+  fs::File,
+  io::{self, Write},
+};
 
 pub fn open_cargo_toml() -> io::Result<File> {
   File::open("Cargo.toml")
@@ -8,17 +11,34 @@ pub fn open_cargo_toml() -> io::Result<File> {
 
 pub fn get_current_version() -> Result<String> {
   let content = read_file(&mut open_cargo_toml()?)?;
+  let content: toml::Value = toml::from_str(&content)?;
 
-  // Find the `version = "x.y.z"` line.
-  let version_line = content
-    .lines()
-    .find(|line| line.contains("version"))
-    .ok_or_else(|| anyhow::anyhow!("'Cargo.toml' is missing 'version' property."))?;
-
-  let version = version_line
-    .split("\"")
-    .nth(1)
+  let version = content
+    .get("package")
+    .and_then(|package| package.get("version"))
+    .and_then(|version| version.as_str())
     .ok_or_else(|| anyhow::anyhow!("'Cargo.toml' is missing 'version' property."))?;
 
   Ok(version.to_string())
+}
+
+/// Edits the `Cargo.toml` file and updates the value of the `version` property.
+pub fn bump_version(version: &str) -> Result<()> {
+  let mut file = open_cargo_toml()?;
+
+  let content = read_file(&mut file)?;
+  let mut content: toml::Value = toml::from_str(&content)?;
+
+  let version_property = content
+    .get_mut("package")
+    .and_then(|package| package.get_mut("version"))
+    .ok_or_else(|| anyhow::anyhow!("'Cargo.toml' is missing 'version' property."))?;
+
+  *version_property = toml::Value::String(version.to_string());
+
+  file.set_len(0)?;
+  file.write_all(content.to_string().as_bytes())?;
+  file.flush()?;
+
+  Ok(())
 }
